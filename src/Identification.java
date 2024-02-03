721f95bb610f04bdddf84678a11f248d0fe7e03b
shreySaxena <shreyyerhs07@gmail.com>
2024-02-04 00:34:03 +0530

    added ignore

---
+++ src/Identification.java
@@ -0,0 +1,7 @@
+public class Identification {
+    //if k is  and smallest/largest is given  then heap is applied
+    //if k+smallest given=apply max heap
+    // if k+largest give=apply min heap
+    //heap ke question generally sorting ke question hote hai.And sorting mai nlogn time lgta hai.But using heap we can do it in nlogk
+
+}
---
+++ src/KthSmallestElement.java
@@ -0,0 +1,38 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class KthSmallestElement {
+    //you can simply sort an array in nlogn and return arr[k-1]
+
+    //use max heap
+    static int func(int arr[],int k){
+
+        //create max heap
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < arr.length; i++) {
+            //add current element
+            maxHeap.add(arr[i]);
+
+            //if size of heap exceeds k then remove element
+            //basically the idea is ki suppose k=5.
+            //toh jab tak maxheap ka size <=5 hai tab tak usme array ka max element store hoga top pr.
+            //[3,3,4,67,76]
+            //ab jab i=5 hoga toh 89 insert hoga
+            //[3,3,4,67,76,89] and since apan 5th smallest element chaiye hai and abhi heap ko dekh kr lag rha hai ki 89
+            // pakka 5th smallest element toh nhi hai, kiuki abhi poora array bhi iterate nhi hua still it is 6th smallest,
+            //agar in future 3 se bhi chota element aaya toh 89 7th smallest bn jaaega toh 89 pakka answer nhi hai toh use
+            //nikaal do.
+            //[3,3,4,67,76]. ab iske hisaab se 76 is 5th smallest but abhi confirm nhi hai ki vahi ans hai, kiuki agar 76
+            //se koi bada aaya toh voh ans pakka nhi hoga but agar 3 se koi chota aaya toh 76 6th smaalest bn jaaega
+            if(maxHeap.size()>k){maxHeap.poll();}
+        }
+        return maxHeap.peek();
+
+    }
+    public static void main(String[] args) {
+        int arr[]={3,4,67,3,76,89,43,21,55,99,35,77};
+        System.out.println(func(arr,5));
+
+    }
+}
